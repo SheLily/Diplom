dc6e3c5c5848bab4b17c3c453c3936e3fae00f21
SheLily <63263537+SheLily@users.noreply.github.com>
2020-08-10 09:43:14 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from vkinder import VKinder
-
-if __name__ == '__main__':
-    token = input('Введите токен пользователя')
-    v = VKinder(token)
-    result = v.do()
-    print('Результат сохранен в файл с текущей датой и временем')
